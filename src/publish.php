dabe2f94e681ebee3e99fe6cc72ebb35ddb522d2
Tim Rodger <tim.rodger@gmail.com>
2015-12-05 22:16:40 +0000

    Adds publishing test events

--- src/publish.php
+++ src/publish.php
@@ -8,8 +8,36 @@
  * Publishes events
  */
 
+require_once __DIR__ . '/vendor/autoload.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
 echo "Publishing schedule events\n";
 
-// get scheduled tasks from store
+// get scheduled tasks from store that should be run now
 
-// publish events
+// publish events, one per task
+$channel_name = 'repo-mon.main';
+$queue_host = getenv('RABBITMQ_PORT_5672_TCP_ADDR');
+$queue_port = getenv('RABBITMQ_PORT_5672_TCP_PORT');
+
+$connection = new AMQPStreamConnection($queue_host, $queue_port, 'guest', 'guest');
+$channel = $connection->channel();
+$channel->queue_declare($channel_name, false, false, false, false);
+
+$event = [
+    'name' => 'repo-mon.repo.update.scheduled',
+    'data' => [
+        'repository' => 'test/test'
+    ]
+];
+
+$msg = new AMQPMessage(json_encode($event, JSON_UNESCAPED_SLASHES), [
+    'content_type' => 'application/json',
+    'timestamp' => time()
+]);
+
+$channel->basic_publish($msg, '', $this->channel_name);
+
+$channel->close();
+$connection->close();
