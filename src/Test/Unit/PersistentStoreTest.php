2035cf852ab3622a4a83266601cfc4bc61450807
Tim Rodger <tim.rodger@gmail.com>
2015-12-11 13:30:20 +0000

    Update PersistentStoreTest.php

--- src/Test/Unit/PersistentStoreTest.php
+++ src/Test/Unit/PersistentStoreTest.php
@@ -33,12 +33,12 @@ class PersistentStoreTest extends PHPUnit_Framework_TestCase
             ->getMock();
         $client->expects($this->once())
             ->method('prepare')
-            ->with('INSERT INTO :table (name, hour, minute, frequency, timezone, data) VALUES(:name, :hour, :minute, :frequency, :timezone, :data)')
+            ->with('INSERT INTO tasks (name, hour, minute, frequency, timezone, data) VALUES(:name, :hour, :minute, :frequency, :timezone, :data)')
             ->will($this->returnValue($mock_statement));
 
         $mock_statement->expects($this->once())
             ->method('execute')
-            ->with([':table' => 'tasks', ':name' => $name, ':hour' => $expected_hour, ':minute' => 1, ':frequency' => 1, ':timezone' => 'UTC', ':data' => json_encode($data, JSON_UNESCAPED_SLASHES)]);
+            ->with([':name' => $name, ':hour' => $expected_hour, ':minute' => 1, ':frequency' => 1, ':timezone' => 'UTC', ':data' => json_encode($data, JSON_UNESCAPED_SLASHES)]);
 
         $store->add($name, $hour, $frequency, $timezone, $data);
     }
@@ -81,12 +81,12 @@ class PersistentStoreTest extends PHPUnit_Framework_TestCase
 
         $client->expects($this->once())
             ->method('prepare')
-            ->with('SELECT FROM * :table WHERE hour = :hour and minute = :minute')
+            ->with('SELECT FROM * tasks WHERE hour = :hour and minute = :minute')
             ->will($this->returnValue($mock_statement));
 
         $mock_statement->expects($this->once())
             ->method('execute')
-            ->with([':table' => 'tasks', ':hour' => $expected_hour, ':minute' => $expected_min]);
+            ->with([':hour' => $expected_hour, ':minute' => $expected_min]);
 
         $mock_statement->expects($this->once())
             ->method('fetchAll')
